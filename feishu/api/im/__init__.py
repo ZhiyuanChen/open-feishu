a7a63c3ba282fa59e14de517df1cfbb414bde756
Zhiyuan Chen <this@zyc.ai>
2024-07-27 04:01:20 +0800

    support basic message
    Signed-off-by: Zhiyuan Chen <this@zyc.ai>

--- feishu/api/im/__init__.py
+++ feishu/api/im/__init__.py
@@ -14,12 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import annotations
+from .messages import get_message, send_message, update_message
 
-APP_ID: str
-APP_SECRET: str
-BASE_URL = "https://open.feishu.cn/open-apis/"
-ACCESS_TOKEN_REFRESH_OFFSET = 5400
-MAX_RETRIES = 3
-BACKOFF_FACTOR = 4
-STREAMING_STATUS_TEXT = "生成中..."
+__all__ = ["send_message", "update_message", "get_message"]
